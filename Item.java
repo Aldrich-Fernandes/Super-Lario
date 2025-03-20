f3fbb5108cc3ab02435a15cda995514a8e265ad9
Fadi Mostefai <fadimostefai@gmail.com>
2025-03-20 17:53:46 +0000

    Added the Item superclass, and made Coin and Key subclasses of this.
    Might add the Tile class has a subclass of the Item class later on...

---
+++ Item.java
@@ -0,0 +1,104 @@
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.animation.*;
+import javafx.util.Duration;
+import javafx.scene.shape.Circle;
+
+public abstract class Item {
+    private boolean collected = false;
+    private String type;
+    private Rectangle key;
+    private Circle coin;
+    
+    public Item(String type, double x, double y) {
+        this.type = type;
+        setType(x, y);
+    }
+    
+    private void setType(double x, double y) {
+        switch (type) {
+            case "KEY":
+                key = new Rectangle(GameMap.TILE_SIZE/2, GameMap.TILE_SIZE/2 * 1.5);
+                
+                // Position the key (Rectangle coordinates are top-left, not center)
+                key.setX(x - GameMap.TILE_SIZE/4);
+                key.setY(y - GameMap.TILE_SIZE/2 * 0.75);
+                
+                // Set appearance
+                key.setFill(Color.GOLD);
+                key.setStroke(Color.DARKORANGE);
+                key.setStrokeWidth(2);
+                key.setArcWidth(GameMap.TILE_SIZE * 0.5);  // Rounded corners
+                key.setArcHeight(GameMap.TILE_SIZE * 0.5);
+                
+                break;
+            
+            case "COIN":
+                coin = new Circle(x, y, GameMap.TILE_SIZE/3);
+                // Set appearance
+                coin.setFill(Color.GOLD);
+                coin.setStroke(Color.DARKGOLDENROD);
+                coin.setStrokeWidth(2);
+        }
+    }
+    
+    /**
+     * Checks if this key has been collected.
+     * 
+     * @return true if the key has been collected, false otherwise
+     */
+    public boolean isCollected() {
+        return collected;
+    }
+    
+    /**
+     * Collects this key, making it invisible.
+     */
+    public void collect() {
+        collected = true;
+        collectionAnimation();
+    }
+    
+    private void collectionAnimation(){
+        if (type.equals("KEY")) {
+            TranslateTransition ascend = new TranslateTransition(Duration.millis(300), key);
+            TranslateTransition fall = new TranslateTransition(Duration.millis(300), key);
+            FadeTransition fade = new FadeTransition(Duration.millis(300), key);
+            ParallelTransition fadeOut = new ParallelTransition(fall, fade); 
+            
+            ascend.setByY(-20);
+            fall.setByY(30);
+            fade.setFromValue(1);
+            fade.setToValue(0);
+            
+            SequentialTransition transition = new SequentialTransition(ascend,fadeOut);
+            transition.setOnFinished(event -> key.setVisible(false));
+            transition.play();
+        }
+        else {
+            TranslateTransition ascend = new TranslateTransition(Duration.millis(300), coin);
+            TranslateTransition fall = new TranslateTransition(Duration.millis(300), coin);
+            FadeTransition fade = new FadeTransition(Duration.millis(300), coin);
+            ParallelTransition fadeOut = new ParallelTransition(fall, fade); 
+            
+            ascend.setByY(-20);
+            fall.setByY(30);
+            fade.setFromValue(1);
+            fade.setToValue(0);
+            
+            SequentialTransition transition = new SequentialTransition(ascend,fadeOut);
+            transition.setOnFinished(event -> coin.setVisible(false));
+            transition.play();
+        }
+    }
+    
+    public abstract boolean checkCollection(Player player);
+    
+    protected Rectangle getKey() {
+        return key;
+    }
+    
+    protected Circle getCoin() {
+        return coin;
+    }
+}
--- Key.java
+++ Key.java
@@ -2,12 +2,12 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.animation.*;
 import javafx.util.Duration;
+
 /**
  * Key class represents a key that can be collected in the game.
  * It is represented by a simple rectangle with a key-like shape.
  */
-public class Key extends Rectangle {
-    private boolean collected = false;
+public class Key extends Item {
     private int requiredCoins = 10;
     
     /**
@@ -17,54 +17,10 @@ public class Key extends Rectangle {
      * @param centerY The y-coordinate of the center of the key
      * @param size The size of the key
      */
-    public Key(double centerX, double centerY, double size) {
-        super(size, size * 1.5);
-        
-        // Position the key (Rectangle coordinates are top-left, not center)
-        setX(centerX - size/2);
-        setY(centerY - size * 0.75);
-        
-        // Set appearance
-        setFill(Color.GOLD);
-        setStroke(Color.DARKORANGE);
-        setStrokeWidth(2);
-        setArcWidth(size * 0.5);  // Rounded corners
-        setArcHeight(size * 0.5);
+    public Key(double centerX, double centerY) {
+        super("KEY", centerX, centerY);
     }
     
-    /**
-     * Checks if this key has been collected.
-     * 
-     * @return true if the key has been collected, false otherwise
-     */
-    public boolean isCollected() {
-        return collected;
-    }
-    
-    /**
-     * Collects this key, making it invisible.
-     */
-    public void collect() {
-        collected = true;
-        collectionAnimation();
-    }
-    
-    private void collectionAnimation(){
-        TranslateTransition ascend = new TranslateTransition(Duration.millis(300), this);
-        TranslateTransition fall = new TranslateTransition(Duration.millis(300), this);
-        FadeTransition fade = new FadeTransition(Duration.millis(300), this);
-        ParallelTransition fadeOut = new ParallelTransition(fall, fade);
-        
-        ascend.setByY(-20);
-        fall.setByY(30);
-        fade.setFromValue(1);
-        fade.setToValue(0);
-        
-        SequentialTransition transition = new SequentialTransition(ascend,fadeOut);
-        transition.setOnFinished(event -> setVisible(false));
-        transition.play();
-    }
-
     /**
      * Returns the amount of coins necessary to pickup the key!
      */
@@ -78,15 +34,18 @@ public class Key extends Rectangle {
      * @param player The player to check against
      * @return true if the key should be collected, false otherwise
      */
+    @Override
     public boolean checkCollection(Player player) {
-        if (collected) return false;
+        if (super.isCollected()) return false;
+        
+        Rectangle key = super.getKey();
         
         // Calculate distance between player center and key center
-        double keyBounds = Math.max(getWidth(), getHeight()) / 2;
+        double keyBounds = Math.max(key.getWidth(), key.getHeight()) / 2;
         double playerX = player.getCenterX();
         double playerY = player.getCenterY();
-        double keyX = getX() + getWidth()/2;
-        double keyY = getY() + getHeight()/2;
+        double keyX = key.getX() + key.getWidth()/2;
+        double keyY = key.getY() + key.getHeight()/2;
         
         double dx = playerX - keyX;
         double dy = playerY - keyY;
